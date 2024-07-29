8d08da736c5c5da7242a8c10be3997096f7b7a1d
Petr Brabec <dev@petrbrabec.cz>
2024-07-29 19:29:17 +0300

    fix: 404 for pokemon detail

--- frontend/src/components/PokemonDetail.tsx
+++ frontend/src/components/PokemonDetail.tsx
@@ -85,105 +85,107 @@ export const PokemonDetail = () => {
           <Flex align="center" justify="center" h={rem(400)} flex={1}>
             <Loader color="red" />
           </Flex>
+        ) : pokemon == null ? (
+          <Container size="xs" mt="xl" mb="xl">
+            <Text ta="center">Pokemon not found</Text>
+          </Container>
         ) : (
-          pokemon && (
-            <>
-              <Container size="xs" mt="xl" mb="xl">
-                <PokemonImage {...pokemon} />
+          <>
+            <Container size="xs" mt="xl" mb="xl">
+              <PokemonImage {...pokemon} />
 
-                <Group justify="space-between" mt="md" gap="xs" wrap="nowrap">
-                  <Title>{pokemon.name}</Title>
-                  <Group w={rem(36)} justify="center">
-                    <PokemonFavoriteButton {...pokemon} />
-                  </Group>
+              <Group justify="space-between" mt="md" gap="xs" wrap="nowrap">
+                <Title>{pokemon.name}</Title>
+                <Group w={rem(36)} justify="center">
+                  <PokemonFavoriteButton {...pokemon} />
                 </Group>
+              </Group>
 
-                <Group justify="space-between" mt="md">
-                  <Group gap="xs">
-                    {pokemon.types.map((type) => (
-                      <Link
-                        key={type}
-                        href={Path.Pokedex({ type: type as PokemonType })}
+              <Group justify="space-between" mt="md">
+                <Group gap="xs">
+                  {pokemon.types.map((type) => (
+                    <Link
+                      key={type}
+                      href={Path.Pokedex({ type: type as PokemonType })}
+                    >
+                      <Badge
+                        color={PokemonTypeColors[type as PokemonType]}
+                        size="lg"
                       >
-                        <Badge
-                          color={PokemonTypeColors[type as PokemonType]}
-                          size="lg"
-                        >
-                          {type}
-                        </Badge>
-                      </Link>
+                        {type}
+                      </Badge>
+                    </Link>
+                  ))}
+                </Group>
+                <PokemonAudioButton id={pokemon.id} />
+              </Group>
+
+              <Stack mt="md">
+                <Progress.Root size="xl">
+                  <Progress.Section
+                    color="violet"
+                    value={(pokemon.maxCP / PokemonStaticData.maxCP) * 100}
+                  >
+                    <Progress.Label>CP: {pokemon.maxCP}</Progress.Label>
+                  </Progress.Section>
+                </Progress.Root>
+
+                <Progress.Root size="xl">
+                  <Progress.Section
+                    color="green"
+                    value={(pokemon.maxHP / PokemonStaticData.maxHP) * 100}
+                  >
+                    <Progress.Label>HP: {pokemon.maxHP}</Progress.Label>
+                  </Progress.Section>
+                </Progress.Root>
+              </Stack>
+
+              <Group mt="xl">
+                <Stack gap="xs" flex={1}>
+                  <Title order={4} ta="center">
+                    Height
+                  </Title>
+                  <Text ta="center">
+                    {pokemon.height.minimum} - {pokemon.height.maximum}
+                  </Text>
+                </Stack>
+                <Stack gap="xs" flex={1}>
+                  <Title order={4} ta="center">
+                    Weight
+                  </Title>
+                  <Text ta="center">
+                    {pokemon.weight.minimum} - {pokemon.weight.maximum}
+                  </Text>
+                </Stack>
+              </Group>
+            </Container>
+
+            {pokemon.evolutions.length > 0 && (
+              <>
+                <Divider />
+
+                <Container py="xl">
+                  <Title ta="center">Evolutions</Title>
+
+                  <Group mt="md" gap="md" justify="center">
+                    {pokemon.evolutions.map((evolution) => (
+                      <Box
+                        key={evolution.name}
+                        flex={1}
+                        w={rem(500)}
+                        maw={{
+                          base: undefined,
+                          sm: "50%",
+                        }}
+                      >
+                        <PokemonCard {...evolution} />
+                      </Box>
                     ))}
                   </Group>
-                  <PokemonAudioButton id={pokemon.id} />
-                </Group>
-
-                <Stack mt="md">
-                  <Progress.Root size="xl">
-                    <Progress.Section
-                      color="violet"
-                      value={(pokemon.maxCP / PokemonStaticData.maxCP) * 100}
-                    >
-                      <Progress.Label>CP: {pokemon.maxCP}</Progress.Label>
-                    </Progress.Section>
-                  </Progress.Root>
-
-                  <Progress.Root size="xl">
-                    <Progress.Section
-                      color="green"
-                      value={(pokemon.maxHP / PokemonStaticData.maxHP) * 100}
-                    >
-                      <Progress.Label>HP: {pokemon.maxHP}</Progress.Label>
-                    </Progress.Section>
-                  </Progress.Root>
-                </Stack>
-
-                <Group mt="xl">
-                  <Stack gap="xs" flex={1}>
-                    <Title order={4} ta="center">
-                      Height
-                    </Title>
-                    <Text ta="center">
-                      {pokemon.height.minimum} - {pokemon.height.maximum}
-                    </Text>
-                  </Stack>
-                  <Stack gap="xs" flex={1}>
-                    <Title order={4} ta="center">
-                      Weight
-                    </Title>
-                    <Text ta="center">
-                      {pokemon.weight.minimum} - {pokemon.weight.maximum}
-                    </Text>
-                  </Stack>
-                </Group>
-              </Container>
-
-              {pokemon.evolutions.length > 0 && (
-                <>
-                  <Divider />
-
-                  <Container py="xl">
-                    <Title ta="center">Evolutions</Title>
-
-                    <Group mt="md" gap="md" justify="center">
-                      {pokemon.evolutions.map((evolution) => (
-                        <Box
-                          key={evolution.name}
-                          flex={1}
-                          w={rem(500)}
-                          maw={{
-                            base: undefined,
-                            sm: "50%",
-                          }}
-                        >
-                          <PokemonCard {...evolution} />
-                        </Box>
-                      ))}
-                    </Group>
-                  </Container>
-                </>
-              )}
-            </>
-          )
+                </Container>
+              </>
+            )}
+          </>
         )}
       </AppShell.Main>
     </AppShell>
